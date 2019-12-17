ed641eb16103b5c7cc0716308a1d5faf17b1c880
Erik Larsson <erik.mattias.larsson@gmail.com>
2019-12-17 18:36:56 +0100

    tweaking animation

--- src/startAnimation.js
+++ src/startAnimation.js
@@ -1,12 +1,18 @@
 import * as PIXI from 'pixi.js'
 import * as TWEEN from 'tween.js'
 
-let app, p1, p2, pvp, last, coords, tweener, done
+const centerX = Math.round(window.innerWidth / 2)
+const centerY = Math.round(window.innerHeight / 2)
+
+let app, pvp, last, done, backgroundGraphics
+
+let p1sprite, p1coord, p1tween
+let p2sprite, p2coord, p2tween
 
 function gameLoop(time) {
   if (done === false) requestAnimationFrame(gameLoop)
-  p1.x = coords.p1
-  p2.x = coords.p2
+  p1sprite.x = p1coord.x
+  p2sprite.x = p2coord.x
   TWEEN.update(time)
 }
 
@@ -18,30 +24,47 @@ function setup() {
     app.view.remove()
   }, 2000)
 
-  p1 = new PIXI.Sprite.from('battle-stance/leftfacing/bowser.png')
-  p1.x = 0
-  p1.y = 0
-  app.stage.addChild(p1)
+  rect()
 
-  p2 = new PIXI.Sprite.from('battle-stance/rightfacing/yoshi.png')
-  p2.x = 0
-  p2.y = 0
-  app.stage.addChild(p2)
+  p1sprite = new PIXI.Sprite.from('battle-stance/leftfacing/bowser.png')
+  p1sprite.x = 0
+  p1sprite.y = 0
+  app.stage.addChild(p1sprite)
+
+  p2sprite = new PIXI.Sprite.from('battle-stance/rightfacing/yoshi.png')
+  p2sprite.x = 0
+  p2sprite.y = 0
+  app.stage.addChild(p2sprite)
 
   pvp = new PIXI.Sprite.from('player-versus-player.png')
-  pvp.x = Math.round(window.innerWidth / 2) - 82
-  pvp.y = Math.round(window.innerHeight / 2) - 82
+  pvp.x = centerX - 82
+  pvp.y = centerY - 82
   app.stage.addChild(pvp)
 
   last = new Date().getTime()
 
-  coords = { p1: -750, p2: window.innerWidth }
-  tweener = new TWEEN.Tween(coords)
-    .to({ p1: window.innerWidth - 600, p2: -150 }, 600)
+  p1coord = { x: -750 }
+  p2coord = { x: window.innerWidth }
+
+  p1tween = new TWEEN.Tween(p1coord)
+    .to({ x: centerX + 100 }, 600)
     .easing(TWEEN.Easing.Quadratic.Out)
     .start()
 
+  p2tween = new TWEEN.Tween(p2coord)
+    .to({ x: centerX - 850 }, 600)
+    .easing(TWEEN.Easing.Quadratic.Out)
+    .delay(1000)
+    .start()
+
   gameLoop()
+}
+
+function rect() {
+  backgroundGraphics = new PIXI.Graphics()
+  backgroundGraphics.beginFill(0x000000)
+  backgroundGraphics.drawRect(0, 0, window.innerWidth, window.innerHeight)
+  app.stage.addChild(backgroundGraphics)
 }
 
 export default function startAnimation() {
