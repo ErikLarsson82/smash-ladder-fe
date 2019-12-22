8d5b50f12b88ebd856cc49fd8236a062dc7dff8c
Erik Larsson <erik.mattias.larsson@gmail.com>
2019-12-22 14:53:58 +0100

    Nice trend arrows

--- src/Player.js
+++ src/Player.js
@@ -1,5 +1,10 @@
 import React from 'react'
 import { QcIcon, Mugshot, Icon } from './helpers'
+import KeyboardArrowUpTwoToneIcon from '@material-ui/icons/KeyboardArrowUpTwoTone'
+import KeyboardArrowDownTwoToneIcon from '@material-ui/icons/KeyboardArrowDownTwoTone'
+import ArrowRightAltTwoToneIcon from '@material-ui/icons/ArrowRightAltTwoTone'
+import DoubleArrowIcon from '@material-ui/icons/DoubleArrow'
+import { StylesProvider } from '@material-ui/core/styles'
 
 export default function PlayerRow(props) {
   const { delay, playerslug, name, main, secondary, trend, qc, idx, highlight, highlightPlayer } = props
@@ -21,11 +26,31 @@ export default function PlayerRow(props) {
     <tr
       className={[startClass].concat(delay ? delayedClass : []).join(' ')}
       onClick={() => highlightPlayer(playerslug)}>
-      <td>{trend}</td>
+      <td className="center">
+        <StylesProvider injectFirst>
+          {trendIcon(trend)}
+        </StylesProvider>
+      </td>
       <td><div className="placement">{idx+1}.</div></td>
       <td><Mugshot playerslug={playerslug} />{name}{ qcIcons.length > 0 && qcIcons}</td>
       <td><Icon name={main} /></td>
       <td>{secondary && <Icon name={secondary} />}</td>
     </tr>
   )
+}
+
+function trendIcon(trend) {
+  if (trend > 1)
+    return <DoubleArrowIcon className="trend-up double-up" />
+
+  if (trend > 0)
+    return <KeyboardArrowUpTwoToneIcon className="trend-up" />
+
+  if (trend < -1)
+    return <DoubleArrowIcon className="trend-down double-down" />
+
+  if (trend < 0)
+    return <KeyboardArrowDownTwoToneIcon className="trend-down" />
+
+  return <ArrowRightAltTwoToneIcon className="unchanged" />
 }
