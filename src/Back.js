a3251673aebc1b49be1edc58acc4e21535ec362a
Erik Larsson <erik.mattias.larsson@gmail.com>
2019-12-22 11:18:50 +0100

    Back button

--- src/Back.js
+++ src/Back.js
@@ -1,7 +1,11 @@
 import React from 'react'
+import CancelIcon from '@material-ui/icons/Cancel';
+import { StylesProvider } from '@material-ui/core/styles'
 
 export default function Back({ setscreen }) {
 	return (
-		<div onClick={() => setscreen('DASHBOARD')} className="back">X</div>
+		<StylesProvider injectFirst>
+	      <CancelIcon className="back" onClick={() => setscreen('DASHBOARD')} />
+	    </StylesProvider>
 	)
 }
